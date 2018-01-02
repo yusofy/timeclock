d130bb7109e04b908cdadd32d538327a23544669
Yusof Yaghi <yusofyaghi90@gmail.com>
2018-01-01 21:32:24 -0600

    Removed punchclock

--- punchclock/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (file_exists("menu.php")) {
-    header("Location: menu.php");
-} else if (file_exists("punchclock/menu.php")) {
-    header("Location: punchclock/menu.php");
-} else {
-    header("Location: timeclock.php");
-}
-?>
